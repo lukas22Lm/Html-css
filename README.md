b57baa32fec29c176e4107bfb26d78f8cc1d3303
lukas22Lm <lukasmatias387@gmail.com>
2025-07-26 20:15:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Html-css
+curso de html 5 e css 3
---
+++ estudos/ex008/script1.js
@@ -0,0 +1,8 @@
+let saida = document.getElementById("saída");
+
+let num1 = parseInt(prompt("digite um numero"));
+let num2 = parseInt(prompt("digite outro numero"));
+
+let soma = num1 + num2
+
+saida.innerHTML
